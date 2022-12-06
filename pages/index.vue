5e1d05efd5ba0484e7777a144d807bb764096d28
Developer-Rezwan <programmerrezwan@gmail.com>
2022-12-06 19:32:58 +0600

    Let Clone the twitter Applciation

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div :class="{ dark : isDark}">
+    <div class="dark:bg-black">
+      <h1 class="dark:text-dim-600">Hello</h1>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const isDark = ref(true);
+</script>
+
+<style scoped>
+</style>
